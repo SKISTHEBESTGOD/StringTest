84d55d1ce2ce8a3f13b018b91e639f018e1ca5bc
SKISTHEBESTGOD <skiperersiema@gmail.com>
2023-11-23 13:26:26 +0100

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Podaj string: ");
+        String ciag = skaner.nextLine();
+        String ciag1 = "";
+        String ciag2 = "";
+        if(ciag.length() >= 2) {
+            ciag1 = ciag.substring((ciag.length() / 2) - 1,(ciag.length() / 2) + 1);
+            ciag2 = ciag1.concat( ciag.substring(0,(ciag.length() / 2) - 1) + ciag.substring((ciag.length() / 2) + 1));
+
+        }
+        else if(ciag.length() == 0) {
+            System.out.println("pusty string");
+        }
+        else {
+            System.out.println("Nieprawidłowa długość stringa");
+        }
+        System.out.println(ciag2);
     }
 }
